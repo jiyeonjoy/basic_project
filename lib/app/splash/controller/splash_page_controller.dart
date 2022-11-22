6ebd78dcba970bd0142e21b7910c8de94a49b577
최지연 <choi02647@naver.com>
2022-11-22 11:47:39 +0900

    root page 이동 추가

--- lib/app/splash/controller/splash_page_controller.dart
+++ lib/app/splash/controller/splash_page_controller.dart
@@ -1,5 +1,17 @@
+import 'package:basic_project/common/app_pages.dart';
 import 'package:get/get.dart';
 
 class SplashPageController extends GetxController {
   static SplashPageController get to => Get.find();
+
+  @override
+  void onReady() {
+    super.onReady();
+    performAppInitialization();
+  }
+
+  void performAppInitialization() async {
+    await Future.delayed(const Duration(milliseconds: 200));
+    Get.offNamed(AppRoutes.rootPage);
+  }
 }
